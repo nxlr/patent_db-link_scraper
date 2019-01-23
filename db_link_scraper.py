6d23620c75eb637c1991ebe82329af0146f122f9
Sachin Sharma <amrahs.nihcas@gmail.com>
2019-01-23 18:54:08 +0530

    testing response after submitting google form

--- db_link_scraper.py
+++ db_link_scraper.py
@@ -43,7 +43,7 @@ google_form = browser.select_form()
 # test whether form is selected or not
 # browser.get_current_form().print_summary()
 
-browser["q"] = "ind vs nz"
+browser["q"] = "india patent database"
 
 response = browser.submit_selected()
 
