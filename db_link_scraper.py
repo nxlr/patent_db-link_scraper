d6da1ace6f579d3bba16599e17ac79d937f602b0
Sachin Sharma <amrahs.nihcas@gmail.com>
2019-01-23 18:40:04 +0530

    add random user-agent functionality using user-agent.txt file

--- db_link_scraper.py
+++ db_link_scraper.py
@@ -6,12 +6,34 @@ import numpy as np
 import mechanicalsoup
 import re
 
+
+# get random user agent 
+def get_random_ua():
+    random_ua = ''
+    ua_file = 'user-agents.txt'
+    try:
+        with open(ua_file) as f:
+            lines = f.readlines()
+        if len(lines) > 0:
+            prng = np.random.RandomState()
+            index = prng.permutation(len(lines) - 1)
+            idx = np.asarray(index, dtype=np.integer)[0]
+            random_ua = lines[int(idx)]
+    except Exception as ex:
+        print('Exception in random_ua')
+        print(str(ex))
+    finally:
+        return random_ua.rstrip()
+
+# create random user_agent
+user_agent = get_random_ua()
+
 url_google = "https://www.google.com/"
 
 browser = mechanicalsoup.StatefulBrowser(
         soup_config={'features': 'lxml'},  # Use the lxml HTML parser
         raise_on_404=True,
-        #user_agent=user_agent,
+        user_agent=user_agent,
 )
 
 browser.open(url_google)
