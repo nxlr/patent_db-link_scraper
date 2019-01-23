dccab3d8ccea4e44d3c00a37e8cbefad78a7ecb9
Sachin Sharma <amrahs.nihcas@gmail.com>
2019-01-24 00:02:07 +0530

    added function to retrieve wipo member countries, function to get top link from google and storing all records in .xls sheet

--- db_link_scraper.py
+++ db_link_scraper.py
@@ -5,6 +5,31 @@ import pandas as pd
 import numpy as np
 import mechanicalsoup
 import re
+import time
+import xlwt 
+from xlwt import Workbook 
+
+# returns all the wipo members as mentioned on the website
+def wipo_members(): 
+    url = "https://www.wipo.int/members/en/"
+
+    browser = mechanicalsoup.StatefulBrowser(
+        soup_config={'features': 'lxml'},  # Use the lxml HTML parser
+        raise_on_404=True,
+        user_agent=user_agent,
+    )
+
+    browser.open(url)
+    page = browser.get_current_page()
+
+    countries_list = page.find("div", {"class": "cols cols--three"}).find_all("li")
+    countries = []
+    
+    for li in countries_list:
+        countries.append(li.get_text())
+
+    browser.close()
+    return countries
 
 
 # get random user agent 
@@ -25,28 +50,86 @@ def get_random_ua():
     finally:
         return random_ua.rstrip()
 
-# create random user_agent
-user_agent = get_random_ua()
+# get the results 
+def get_results(query, num_results, lang_code):
+    assert isinstance(query, str), 'Search term must be a string'
+    assert isinstance(num_results, int), 'Number of results must be an integer'
+    escaped_query = query.replace(' ', '+')
+    google_url = 'https://www.google.com/search?q={}&num={}&hl={}'.format(escaped_query, num_results, lang_code)
+    response = requests.get(google_url, headers=headers)
+    response.raise_for_status()
+    return response.content
 
-url_google = "https://www.google.com/"
+# get the relevant content from soup
+def parse_results(results_html):
+    soup = BeautifulSoup(results_html, 'html.parser')
+    result_block = soup.find_all('div', attrs={'class': 'g'})
+    for result in result_block:
+        link = result.find('a', href=True)
+        title = result.find('h3', attrs={'class': 'r'})
+        if link and title:
+            link_tmp = link['href'][7:]
+            head, sep, tail = link_tmp.partition('&')
+            link = head
+            title = title.get_text()
+            result_dict = {'link': link, 'title': title}
+    return result_dict
 
-browser = mechanicalsoup.StatefulBrowser(
-        soup_config={'features': 'lxml'},  # Use the lxml HTML parser
-        raise_on_404=True,
-        user_agent=user_agent,
-)
+# scrape for results on google
+def scrape_google(query, num_results, lang_code):
+    try:
+        results_html = get_results(query, num_results, lang_code)
+        result = parse_results(results_html)
+        return result
+    except AssertionError:
+        raise Exception("Incorrect arguments parsed to function")
+    except requests.HTTPError:
+        raise Exception("You appear to have been blocked by Google")
+    except requests.RequestException:
+        raise Exception("Appears to be an issue with your connection")
 
-browser.open(url_google)
+# main function
+if __name__ == '__main__':
+    start = time.time()
 
-google_form = browser.select_form()
 
-# test whether form is selected or not
-# browser.get_current_form().print_summary()
+    # create random user_agent
+    user_agent = get_random_ua()
 
-browser["q"] = "india patent database"
+    headers = {
+        'user-agent': user_agent,
+    }
 
-response = browser.submit_selected()
+    # list of countries on wipo website
+    countries = wipo_members()
 
-soup = BeautifulSoup(response.content, 'html.parser')
+    # Excel Workbook is created 
+    wb = Workbook() 
 
-print(soup.prettify())
+    # first sheet created
+    sheet1 = wb.add_sheet('Sheet 1') 
+    serial_number = 0
+
+    for country in countries:
+        query = country + ' patent database'
+
+        # scrape top 1 results for the query
+        result = scrape_google(query, 1, "en")
+
+        link = result.get('link')
+        title = result.get('title')
+
+        #print(country, link, title)
+
+        sheet1.write(serial_number, 0, serial_number+1)
+        sheet1.write(serial_number, 1, country)
+        sheet1.write(serial_number, 2, link)
+        sheet1.write(serial_number, 3, title)
+        print(serial_number)
+        serial_number += 1
+        
+    wb.save('countries_patent-db_links.xls') 
+    end = time.time()
+    print(end - start)
+    
+    
